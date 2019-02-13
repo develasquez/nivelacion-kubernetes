875d0c0138329aa7b5c47c9d8d568d5aefc25add
Felipe Velasquez <felipe.velasquez@globant.com>
2019-02-13 16:44:53 -0300

    5 - Exponemos el nombre de cada pod como variable de entorno

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola perro');
+    res.send({
+        nodo: process.env.MY_NODE_NAME,
+        pod: process.env.MY_POD_NAME
+    });
 });
 
 app.listen(8080, function () {
-  console.log('Listos en el 8080');
+    console.log('Listos en el 8080');
 });
