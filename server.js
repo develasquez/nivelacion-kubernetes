19416ae4c2fccc3dd09310dc7d3fad69aea2b6cf
Felipe Velasquez <felipe.velasquez@globant.com>
2019-02-13 18:22:45 -0300

    7 -revisamos el estado de nuestro servicio

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get('/', function (req, res) {
     });
 });
 
+app.get('/ready', function (req, res) {
+    //Aca tenemos que esperar que todo este inicializado 
+    //Para poder recibir el primer request
+    res.sendStatus(200);
+});
+app.get('/health', function (req, res) {
+    //Este endpoint da una señal mínima de vida 
+    //Tambien puede indicar la salud completa del servicio.
+    //Sigue con acceso a otros sub servicios? Se ve la BD? 
+    res.sendStatus(200);
+});
+
 app.listen(8080, function () {
     console.log('Listos en el 8080');
 });
